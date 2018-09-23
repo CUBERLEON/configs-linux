a4eb314033e868dc3bffaab6f641f6afbd4fb42d
Liubomyr Piadyk <liubomyr.piadyk@gmail.com>
2018-09-24 01:38:08 +0300

    add rofi window switch shortcut + dependencies install

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,6 +2,8 @@ USER_HOME=$HOME
 ROOT=/
 ARGS=-R
 
+sudo pacman -S gvim zsh i3-gaps plasma kde-applications rofi
+
 stow $ARGS -t $USER_HOME git
 stow $ARGS -t $USER_HOME i3-kde
 stow $ARGS -t $USER_HOME konsole
