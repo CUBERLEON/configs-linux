6e86e914e25ca2af074b9102156871e5c108a4d0
Liubomyr Piadyk <liubomyr.piadyk@gmail.com>
2018-10-13 00:24:10 +0300

    add more apps to the install script

--- install_apps.sh
+++ install_apps.sh
@@ -1,14 +1,27 @@
-PACMAN_ARGS=--needed
+ARGS="-S --needed --noconfirm"
 
 # base
-sudo pacman -S $PACMAN_ARGS base-devel git wget yajl
-sudo pacman -S $PACMAN_ARGS gvim zsh most
+sudo pacman $ARGS base-devel git wget gvim ntfs-3g most
+sudo pacman $ARGS yajl stow python-pip rsync htop
 
-#yay
+# yay
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg $PACMAN_ARGS -si
+makepkg --needed -si
+
+# zsh
+yay $ARGS zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # desktop environment
-sudo pacman -S $PACMAN_ARGS i3-gaps plasma kdebase rofi
+yay $ARGS xorg-xinit i3-gaps plasma kdebase
+sudo mv /usr/bin/ksplashqml /usr/bin/ksplashqml.old
+yay $ARGS redshift rofi compton ttf-ms-fonts feh
+sudo pip install i3-cycle
+
+# extra
+yay $ARGS neofetch gparted
+yay $ARGS code jetbrains-toolbox
+yay $ARGS google-chrome telegram-desktop dropbox tixati mpv
+yay $ARGS safeeyes xprintidle
